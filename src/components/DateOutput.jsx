31d0e3612908883e7a459a4476a04c2f0a2401e2
Abrham007 <abrhamaraya111@gmail.com>
2023-11-04 19:48:56 +0300

    finished the code output functionality

--- src/components/DateOutput.jsx
+++ src/components/DateOutput.jsx
@@ -1,18 +1,24 @@
 import React from "react";
 
-function DateOutput() {
+function DateOutput(props) {
   return (
     <div className="date-output">
       <div className="date-output__item">
-        <p className="text-L--alt">- -</p>
+        <p className="text-L--alt">
+          {props.age.years ? props.age.years : "- -"}
+        </p>
         <p className="text-L">years</p>
       </div>
       <div className="date-output__item">
-        <p className="text-L--alt">- -</p>
+        <p className="text-L--alt">
+          {props.age.months ? props.age.months : "- -"}
+        </p>
         <p className="text-L">months</p>
       </div>
       <div className="date-output__item">
-        <p className="text-L--alt">- -</p>
+        <p className="text-L--alt">
+          {props.age.days ? props.age.days : "- -"}{" "}
+        </p>
         <p className="text-L">days</p>
       </div>
     </div>
